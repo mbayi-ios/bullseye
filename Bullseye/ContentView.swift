063ca83c5b0dbb5b32a725879da6ba6714d75b39
mbayi-ios <ambayi@lunchbox.io>
2022-04-07 14:54:10 +0300

    Update ContentView.swift
    - added swiftUI Modifiers to the app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO!")
+                .bold()
+                .kerning(4.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+            }
+            Button(action: {}){
+                Text("Hit Me!")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+
+
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
